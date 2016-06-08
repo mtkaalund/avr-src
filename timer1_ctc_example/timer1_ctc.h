3d93c0720198dadd169a3f3c825cd56572d20cb5
Michael Torp Kaalund <kaalund@gmail.com>
2016-06-09 00:28:59 +0200

    added enum prescaler

--- timer1_ctc_example/timer1_ctc.h
+++ timer1_ctc_example/timer1_ctc.h
@@ -5,6 +5,18 @@
 #include <avr/io.h>
 #include <bits.h>
 
+/*
+ Defines our prescale as a enum, just so the code is more readable
+*/
+typedef enum {
+	STOP,
+	CLK1,
+	CLK8,
+	CLK64,
+	CLK256,
+	CLK1024
+} timer1_prescale;
+
 void timer1_ctc_init(void);
 void timer1_ctc_set(unsigned int foc1a);
 
