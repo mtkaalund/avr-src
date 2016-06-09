aabe24dc6a56bebd8199725139d5d1b74bea12cb
Michael Torp Kaalund <kaalund@gmail.com>
2016-06-09 16:31:41 +0200

    added cli and sei

--- timer1_ctc_example/timer1_ctc.c
+++ timer1_ctc_example/timer1_ctc.c
@@ -19,27 +19,45 @@ void timer1_ctc_set(unsigned int foc1a)
 
 	OCRnA = 16000000 / ( 2 * 64 * 1000 ) - 1 = 
 */
+	cli();		//Disable interrupts
 	OCR1A = foc1a;// 15624;		//Count to
+	sei();		//Enable interrupts
 }
 
 void timer1_ctc_set_prescale( Timer1Prescale pre)
 {
+	cli();		// Disable interrupts
+
 	switch(pre)
 	{
 	case Timer1Prescale.STOP:
+		/* unset CS12 CS11 CS10 */
 		break;
 	case Timer1Prescale.CLK1:
+		/* unset CS12 CS11 set CS10 */
 		break;
 	case Timer1Prescale.CLK8:
+		/* unset CS12 CS10 set CS11 */
 		break;
 	case Timer1Prescale.CLK64:
+		/* unset CS12 set CS11 CS10 */ 
 		TCCR1B |= (BIT(CS10) | BIT(CS12)); //Start timer at Fcpu/64
 		break;
 	case Timer1Prescale.CLK256:
+		/* unset CS11 CS10 set CS12 */
 		break;
 	case Timer1Prescale.CLK1024:
+		/* unset CS11 set CS12 CS10 */
+		break;
+	case Timer1Prescale.EXT_FALL:
+		/* unset CS10 set CS12 CS11 */
+		break;
+	case Timer1Prescale.EXT_RISE:
+		/* set CS12 CS11 CS10 */
 		break;
 	default:
 		break;
 	}
+
+	sei();		// Enable interrupts
 }
