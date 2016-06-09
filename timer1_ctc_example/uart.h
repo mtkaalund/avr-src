de76ebc703438b7acbf22249a0756564b3aeaf22
Michael Torp Kaalund <kaalund@gmail.com>
2016-06-09 22:40:36 +0200

    uart header

---
+++ timer1_ctc_example/uart.h
@@ -0,0 +1,21 @@
+#ifndef UART_H
+#define UART_H
+/* standard c include */
+#include <stdio.h>
+/* AVR-libc include */
+#include <avr/io.h>
+#include <util/setbaud.h>
+/* User-lib include */
+#include <bits.h>
+
+void uart_init( void );
+void uart_rx_interrupt_enable( void );
+void uart_rx_interrupt_disable( void );
+void uart_tx_interrupt_enable( void );
+void uart_tx_interrupt_disable( void );
+void uart_putchar(uint8_t c, FILE *stream);
+uint8_t uart_getchar(FILE *stream);
+
+static FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+static FILE uart_input	= FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
+#endif
