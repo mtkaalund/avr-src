a80ae25d859db8d4a48baa56f03ce6d33e5c21b4
Michael Torp Kaalund <kaalund@gmail.com>
2018-12-04 20:21:32 +0100

    Change the previuse bit_* macros to bit_mask_*, and added bit_* as the correct way of naming them

--- include/bits.h
+++ include/bits.h
@@ -1,10 +1,20 @@
 #ifndef BITS_H_
 #define BITS_H_
 
-#define bit_get(p,m)	((p) & (m))
-#define bit_set(p,m)	((p) |= (m))
-#define bit_clear(p,m)	((p) &= ~(m))
-#define bit_flip(p,m)	((p) ^= (m))
-#define BIT(x)		(0x01 << (x))
+// If _BV is not defined anywhere else
+// it will be defined here.
+#ifndef _BV
+#define _BV(x)		(0x01 << (x))
+#endif
+
+#define bit_mask_get(p,m)	((p) & (m))
+#define bit_mask_set(p,m)	((p) |= (m))
+#define bit_mask_clear(p,m)	((p) &= ~(m))
+#define bit_mask_flip(p,m)	((p) ^= (m))
+
+#define bit_get(p,b)		((p) & (_BV(b)))
+#define bit_set(p,b)		((p) |= (_BV(b)))
+#define bit_clear(p,b)		((p) &= (_BV(b)))
+#define bit_flip(p,b)		((p) ^= (_BV(b)))
 
 #endif
